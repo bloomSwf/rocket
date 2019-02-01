253692d38f3582b696e05ea58c41319954bcf5c8
应俊 <648801714@qq.com>
2019-02-01 13:44:45 +0800

    test yj

--- src/main/java/com/rocket/rocket/controller/RocketMqController.java
+++ src/main/java/com/rocket/rocket/controller/RocketMqController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RocketMqController {
     @RequestMapping("test")
+
     public String test() {
         return null;
     }
