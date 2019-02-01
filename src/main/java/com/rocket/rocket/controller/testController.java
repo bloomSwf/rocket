a5bfeb7afbcf2179f2585d0da85b8969a4346d0e
潘国忠 <panguozhong@wondersgroup.com>
2019-02-01 13:37:26 +0800

    submit

--- src/main/java/com/rocket/rocket/controller/testController.java
+++ src/main/java/com/rocket/rocket/controller/testController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  测试
+ */
 @RestController
 public class testController {
     @Autowired
